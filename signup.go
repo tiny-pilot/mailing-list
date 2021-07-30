a29bfc0f52426f372fa3761adf51cd7cdbf2e5de
Michael Lynch <git@mtlynch.io>
2021-07-30 17:47:57 -0400

    Add CORS and HTTP method checking

--- signup.go
+++ signup.go
@@ -6,6 +6,18 @@ import (
 )
 
 func Signup(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "https://tinypilotkvm.com")
+		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only OPTIONS and POST are supported", http.StatusMethodNotAllowed)
+	}
+
 	var payload struct {
 		Email string `json:"email"`
 	}
@@ -14,5 +26,6 @@ func Signup(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.Header().Set("Access-Control-Allow-Origin", "https://tinypilotkvm.com")
 	// TODO: Finish implementing this.
 }
